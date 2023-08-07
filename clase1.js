f0001ef313a9301fea6ae476fa622b45fed8e322
GonzaSai <136342939+GonzaSai@users.noreply.github.com>
2023-08-07 10:55:28 -0300

    Prueba-Javascript
    Es una prueba de Javascript para el colegio IVT

---
+++ clase1.js
@@ -0,0 +1,52 @@
+//punto 2
+
+console.log('La suma es ' + (20+8))
+console.log ('la resta es ' + (20-8))
+console.log ('la multiplicación es ' + (20*8))
+console.log ('la divisón es ' + (20/8))
+
+//punto 3
+
+let suma= 20+8
+let resta= 20-8
+let multiplicación= 20*8
+let división= 20/8
+
+alert('La suma es ' + suma)
+alert('la resta es ' + resta)
+alert('la multiplicación es ' + multiplicación)
+alert('la divisón es ' + división)
+
+//punto 4
+
+alert('mi edad es ' + (2023-2008))
+
+//punto 5
+
+let edadAlumno
+
+edadAlumno= prompt('Introduce tu edad')
+
+alert('tu edad es ' + edadAlumno)
+
+//punto 6 y 7
+
+let nombreAlumno
+let apellidoAlumno
+
+nombreAlumno= prompt('Introduce tu nombre')
+apellidoAlumno= prompt('Introduce tu apellido (darle a la tecla espacio para que se vea bien)')
+
+alert('Bienvenido ' + (nombreAlumno) + ( apellidoAlumno ))
+
+//punto 8
+
+let diez= 10
+let tres= 3
+let resultadoMultiplicación= diez * tres
+
+console.log(resultadoMultiplicación)
+
+
+
+
